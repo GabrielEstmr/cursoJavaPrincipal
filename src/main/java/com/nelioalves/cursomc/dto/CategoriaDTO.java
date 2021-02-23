db66ad5d192699a14a43ec113ed23ea91ed8e6e4
Gabriel Rodrigues <gabriel.rodrigues@obuc.com.br>
2021-02-23 14:33:35 -0300

    validação sintática com Bean Validation

--- src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
@@ -1,6 +1,12 @@
+//VALIDAÇÕES:CAMADA REST (via DTOS) > acaba que nao é no service
+
 package com.nelioalves.cursomc.dto;
 
 import java.io.Serializable;
+
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
 
 import com.nelioalves.cursomc.domain.Categoria;
 
@@ -8,6 +14,9 @@ public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	//Construtor vazio
